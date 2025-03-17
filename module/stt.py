efbf346a6dc69e5b113b2b9ed8f5af4fb73d85a8
BX-NL <Ninglog_@outlook.com>
2025-03-17 20:46:58 +0800

    Update stt

--- module/stt.py
+++ module/stt.py
@@ -1,11 +1,21 @@
 import pyaudio
 import wave
 import keyboard
-import tempfile
 from funasr import AutoModel
 from funasr.utils.postprocess_utils import rich_transcription_postprocess
-# import config
+
+import os
+import sys
+# demo 模块单独运行时找不到module就用这几行
+# 获取当前文件的绝对路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 获取项目根目录
+work_dir = os.path.dirname(current_dir)
+# 将项目根目录添加到sys.path
+sys.path.append(work_dir)
+# 导入模块
 from module.config import setting
+# from module.core import Core
 
 
 class STT():
@@ -24,31 +34,6 @@ class STT():
         self.RATE = 16000              # 采样率
         self.CHUNK = 1024              # 每个数据块包含的帧数
 
-    # backup
-    # def save_and_transcribe(self, frames):
-    #     # 保存录音到临时文件并进行语音转文字
-    #     with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as tmpfile:
-    #         wave_file = wave.open(tmpfile.name, 'wb')
-    #         wave_file.setnchannels(self.CHANNELS)
-    #         wave_file.setsampwidth(pyaudio.PyAudio().get_sample_size(self.FORMAT))
-    #         wave_file.setframerate(self.RATE)
-    #         wave_file.writeframes(b''.join(frames))
-    #         wave_file.close()
-
-    #         # 语音转文字
-    #         response = self.model.generate(
-    #             input=tmpfile.name,
-    #             cache={},
-    #             language='auto',
-    #             use_itn=True,
-    #             batch_size_s=60,
-    #             merge_vad=True,
-    #             merge_length_s=15,
-    #         )
-    #         # 获取返回结果
-    #         text = rich_transcription_postprocess(response[0]['text'])
-    #         print('识别结果:', text)
-    #         return text
 
     def save_and_transcribe(self, audio_path):
         # 语音转文字
@@ -67,52 +52,128 @@ class STT():
         return text
 
 
-if __name__ == '__main__':
+def debug():
     # demo 此处仅用于测试
-    # ! 312重置后不能使用，有空再改
-    stt = STT()
-    # 录音设置 # todo 存在重复，以后再优化
-    FORMAT = pyaudio.paInt16  # 16位音频格式
-    CHANNELS = 1              # 单声道
-    RATE = 16000              # 采样率
-    CHUNK = 1024              # 每个数据块包含的帧数
+    import time
+    import tempfile
 
-    # 系统设置
+    # 初始化STT
+    stt = STT()
+    # 初始化音频库
+    audio = pyaudio.PyAudio()
+
+    # 存储用户输入的文本
+    user_messsage = ''
+    # 判断是否退出
+    exit_program = False
+
+    # 录音设置 # todo 考虑移到setting里
+    # 16位音频格式、单声道、采样率、每个数据块包含的帧数
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    CHUNK = 1024
+
+    # 读取系统设置，快捷键
     hotkey = setting().get('hotkey')
 
-    # 录音功能，按下指定按键开始和结束录音
-    audio = pyaudio.PyAudio()
+    # 音频帧
     frames = []
+    # 音频流
     stream = None
+    # 标记录音状态
     is_recording = False
-    print('点击', hotkey, '键开始录音，再次按下', hotkey, '键结束录音。')
+    # 初始化按键时长
+    last_press_time = 0
+    # 按键冷却时间
+    debounce_time = 0.5
 
+    print('waiting...')
     try:
-        while True:
+        while not exit_program:
+            # 判断按下快捷键开始或停止录音
             if keyboard.is_pressed(hotkey=hotkey):
-                if not is_recording:
-                    # 开始录音
-                    print('开始录音...')
-                    is_recording = True
-                    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                                        rate=RATE, input=True, frames_per_buffer=CHUNK)
-                    frames = []
-                else:
-                    # 结束录音
-                    print('录音结束.')
-                    is_recording = False
-                    break
+                # 按键冷却，避免误触
+                current_time = time.time()
+                if current_time - last_press_time > debounce_time:
+                    last_press_time = current_time
+                    
+                    if not is_recording:
+                        # 开始录音
+                        print('开始录音')
+                        # 标记录音状态
+                        is_recording = True
+                        # 开始记录音频流
+                        stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+                        # 清空上一次录音留下的音频帧
+                        frames = []
+                    else:
+                        # 结束录音
+                        print('录音结束')
+                        # 标记录音状态
+                        is_recording = False
+                        # with input_lock:
+                        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as tmpfile:
+                            wave_file = wave.open(tmpfile.name, 'wb')
+                            wave_file.setnchannels(CHANNELS)
+                            wave_file.setsampwidth(audio.get_sample_size(FORMAT))
+                            wave_file.setframerate(RATE)
+                            wave_file.writeframes(b''.join(frames))
+                            wave_file.close()
+                        # 语音转文字
+                            # user_messsage = core.transcribe_audio(tmpfile.name)
+                            user_messsage = stt.save_and_transcribe(tmpfile.name)
+                        # text = core.transcribe_audio(frames)
+                        print('get:', user_messsage)
+                        # || 这个break不知道干啥用的，先标记一下
+                        # break
 
+            # 判断是否录音成功
             if is_recording and stream is not None:
+                # 记录音频数据
                 data = stream.read(CHUNK)
                 frames.append(data)
 
     finally:
+        # 用于结束录音
         if stream:
             stream.stop_stream()
             stream.close()
+        # 终止录音，关闭麦克风
         audio.terminate()
 
-    print('开始转换')
-    text = stt.save_and_transcribe(frames)
-    print(text)
+def api():
+    from fastapi import FastAPI, File, UploadFile, HTTPException
+    from pydantic import BaseModel
+
+    app = FastAPI()
+    # 创建 Model 实例
+    stt = STT()
+
+    @app.post('/stt')
+    async def save_and_transcribe_api(tmpfile: UploadFile = File(...)):
+        try:
+            # ? 用不了相对路径，有空再研究
+            tmpfile_path = f'D:/{tmpfile.filename}'
+            # 保存文件到临时路径
+            with open(tmpfile_path, 'wb') as file:
+                file.write(await tmpfile.read())
+                if file:
+                    print('get:', file.name)
+                    pass
+            # 语音转文字
+            user_message = stt.save_and_transcribe(tmpfile_path)
+            return {'user_message': user_message}
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8503)
+
+
+if __name__ == '__main__':
+    if True:
+        api()
+    else:
+        debug()
