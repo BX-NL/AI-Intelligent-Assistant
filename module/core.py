a4768ff45d282e625a25336576a621f8401e7864
BX-NL <Ninglog_@outlook.com>
2025-04-29 19:02:42 +0800

    Update core.py

--- module/core.py
+++ module/core.py
@@ -1,120 +1,95 @@
 import requests
-# todo 控制台日志
 import logging
 # import colorlog
-from .config import setting
 # logging配置
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-# 读取系统设置
-settings = setting()
-# 读取分布式设置
-settings_distribute = settings.get('distribute')
-# 判断是否需要导入本地模块
-if settings_distribute == False:
-    # 导入模块
-    from .stt import STT
-    from .tts import TTS
-    from .control import Control
-
-    # 读取大模型设置
-    settings_model = settings.get('model')
-    if settings_model['LLM'] == 'offline':
-        from .model_offline import Model
-    elif settings_model['LLM'] == 'online':
-        from .model_online import Model
-    else:
-        logging.error('Error Import: model')
-
+# 系统核心不能以此方式运行
+if __name__ == '__main__':
+    logging.error('Error Running: 系统核心不能以此方式运行')
+    input()
 
 class Core:
     def __init__(self):
+        # 导入系统设置
+        from .config import setting
+        # 读取系统设置
+        settings = setting()
+        # 读取分布式设置
+        settings_distribute = settings.get('distribute')
         # 读取各模块设置
         self.settings_model = settings.get('model')
         self.settings_stt = settings.get('STT')
         self.settings_tts = settings.get('TTS')
         self.settings_control = settings.get('control')
-        if settings_distribute == True:
+
+        # 判断是否启用了分布式功能
+        if settings_distribute == False:
+            # 覆写分布式设置
+            self.distribute_model = 'offline'
+            self.distribute_stt = 'offline'
+            self.distribute_tts = 'offline'
+            self.distribute_control = 'offline'
+
+        elif settings_distribute == True:
             # 读取分布式设置
             self.distribute_model = self.settings_model['mode']
             self.distribute_stt = self.settings_stt['mode']
             self.distribute_tts = self.settings_tts['mode']
             self.distribute_control = self.settings_control['mode']
 
-            # 初始化各模块，获取各模块分布式接口
-            if self.distribute_model == 'offline':
-                # import
-                self.model = Model()
-                logging.info('Module Local Model')
-            elif self.distribute_model == 'online':
-                IP = self.settings_model['IP']
-                port = str(self.settings_model['port'])
-                self.url_model = 'http://' + IP + ':' + port + '/model'
-                logging.info('Module Distribute Model: ' + IP + ':' + port)
-
-            if self.distribute_stt == 'offline':
-                # import
-                self.stt = STT()
-                logging.info('Module Local STT')
-            elif self.distribute_stt == 'online':
-                IP = self.settings_stt['IP']
-                port = str(self.settings_stt['port'])
-                self.url_stt = 'http://' + IP + ':' + port + '/stt'
-                logging.info('Module Distribute STT: ' + IP + ':' + port)
-
-            if self.distribute_tts == 'offline':
-                # import
-                self.tts = TTS()
-                logging.info('Module Local TTS')
-            elif self.distribute_tts == 'online':
-                IP = self.settings_tts['IP']
-                port = str(self.settings_tts['port'])
-                self.url_tts = 'http://' + IP + ':' + port + '/tts'
-                logging.info('Module Distribute TTS: ' + IP + ':' + port)
-
-            if self.distribute_control == 'offline':
-                # ! 有bug，不能这么写
-                # from .control import Control
-                self.control = Control()
-                logging.info('Module Local Control: ')
-            elif self.distribute_control == 'online':
-                IP = self.settings_control['IP']
-                port = str(self.settings_control['port'])
-                self.url_control = 'http://' + IP + ':' + port + '/control'
-                logging.info('Module Distribute Control: ' + IP + ':' + port)
-
-        else:
-            # 覆写分布式设置
-            self.distribute_model = 'offline'
-            self.distribute_stt = 'offline'
-            self.distribute_tts = 'offline'
-            self.distribute_control = 'offline'
-            # 初始化各模块
+        # 判断各模块分布式是否启用，初始化各模块或配置分布式接口
+        # 初始化大模型处理模块
+        if self.distribute_model == 'offline':
+            if self.settings_model['LLM'] == 'offline':
+                from .model_offline import Model
+            elif self.settings_model['LLM'] == 'online':
+                from .model_online import Model
+            else:
+                logging.error('Error Import: model')
             self.model = Model()
-            self.stt = STT()
-            self.tts = TTS()
-            self.control = Control()
+            logging.info('Module Local Model')
+        elif self.distribute_model == 'online':
+            IP = self.settings_model['IP']
+            port = str(self.settings_model['port'])
+            self.url_model = 'http://' + IP + ':' + port + '/model'
+            logging.info('Module Distribute Model: ' + IP + ':' + port)
 
-    def transcribe_audio(self, audio_path):
-        logging.info('语音识别中')
+        # 初始化语音转文字模块
         if self.distribute_stt == 'offline':
-            text = self.stt.save_and_transcribe(audio_path)
-
+            from .stt import STT
+            self.stt = STT()
+            logging.info('Module Local STT')
         elif self.distribute_stt == 'online':
-            # 以二进制模式打开文件
-            with open(audio_path, 'rb') as tmpfile:
-                # 使用 multipart/form-data 格式上传文件
-                file = {'tmpfile': tmpfile}
-                # 连接至STT的分布式接口
-                response = requests.post(self.url_stt, files=file)
-                tmpfile.close()
-            # 获取转换后的文字
-            text = response.json()['user_message']
-        else:
-            logging.error('Error Setting: STT')
+            IP = self.settings_stt['IP']
+            port = str(self.settings_stt['port'])
+            self.url_stt = 'http://' + IP + ':' + port + '/stt'
+            logging.info('Module Distribute STT: ' + IP + ':' + port)
 
-        return text
+        # 初始化文字转语音模块
+        if self.distribute_tts == 'offline':
+            from .tts import TTS
+            self.tts = TTS()
+            logging.info('Module Local TTS')
+        elif self.distribute_tts == 'online':
+            IP = self.settings_tts['IP']
+            port = str(self.settings_tts['port'])
+            self.url_tts = 'http://' + IP + ':' + port + '/tts'
+            logging.info('Module Distribute TTS: ' + IP + ':' + port)
 
+        # 初始化系统控制模块
+        if self.distribute_control == 'offline':
+            from .control import Control
+            self.control = Control()
+            logging.info('Module Local Control')
+        elif self.distribute_control == 'online':
+            IP = self.settings_control['IP']
+            port = str(self.settings_control['port'])
+            self.url_control = 'http://' + IP + ':' + port + '/control'
+            logging.info('Module Distribute Control: ' + IP + ':' + port)
+
+    # 配置各模块对应功能
+    # 配置大模型处理模块-提示词注入
     def get_in_prompt(self):
         if self.distribute_model == 'offline':
             history = self.model.in_prompt()
@@ -128,6 +103,7 @@ class Core:
 
         return history
 
+    # 配置大模型处理模块-大模型推理
     def generate_response(self, history, text):
         logging.info('大模型推理中')
         if not text:
@@ -148,6 +124,28 @@ class Core:
 
         return new_message, self.history
 
+    # 配置语音转文字
+    def transcribe_audio(self, audio_path):
+        logging.info('语音识别中')
+        if self.distribute_stt == 'offline':
+            text = self.stt.save_and_transcribe(audio_path)
+
+        elif self.distribute_stt == 'online':
+            # 以二进制模式打开文件
+            with open(audio_path, 'rb') as tmpfile:
+                # 使用 multipart/form-data 格式上传文件
+                file = {'tmpfile': tmpfile}
+                # 连接至STT的分布式接口
+                response = requests.post(self.url_stt, files=file)
+                tmpfile.close()
+            # 获取转换后的文字
+            text = response.json()['user_message']
+        else:
+            logging.error('Error Setting: STT')
+
+        return text
+
+    # 配置文字转语音
     def synthesize(self, text):
         logging.info('语音合成中')
         if self.distribute_tts == 'offline':
@@ -164,6 +162,7 @@ class Core:
 
         return audio_data_base64
 
+    # 配置系统控制模块
     def system_control(self, text):
         if self.distribute_control == 'offline':
             type, message = self.control.extract_message(text)
@@ -176,8 +175,9 @@ class Core:
         else:
             logging.error('Error Setting: Control')
 
+    # 用于获取各模块运行状态
+    # 脑子转不过来了，有空再弄，先这么跑着吧
     def get_module_status(self):
-        # 脑子转不过来了，有空再弄，先这么跑着吧
         status = []
 
         # model
@@ -237,7 +237,3 @@ class Core:
         status.append(statu)
 
         return status
-
-
-if __name__ == '__main__':
-    logging.error('Error Running: Core')
