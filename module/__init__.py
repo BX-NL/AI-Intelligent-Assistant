981df077e23edba01ba6a4118d589c6ff8e0f32a
BX-NL <Ninglog_@outlook.com>
2025-05-28 18:36:54 +0800

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -1,8 +1,1 @@
 __all__ = ['config', 'control', 'core', 'model_offline', 'model_online', 'stt', 'tts']
-# from .config import setting
-# from .control import Control
-# from .core import Core
-# from .model_offline import Model as Model_offline
-# from .model_online import Model as Model_online
-# from .stt import STT
-# from .tts import TTS
